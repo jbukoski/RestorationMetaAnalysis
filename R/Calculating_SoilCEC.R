e7027c954c8f154502d5c04771f308cfa3eb95ce
Felipe Barros <felipe.b4rros@gmail.com>
2018-08-06 16:10:49 -0300

    Firs commit add R soil CEC

---
+++ R/Calculating_SoilCEC.R
@@ -0,0 +1,15 @@
+# Cation exchange capacity of soil in cmolc/kg on four different soil depth (0,5,15,30 cm)
+# install.packages("raster")
+library(raster)
+
+s1 <- raster("../Data/SoilGrid/CECSOL_M_sl1_1km_ll.tif")
+s2 <- raster("../Data/SoilGrid/CECSOL_M_sl2_1km_ll.tif")
+s3 <- raster("../Data/SoilGrid/CECSOL_M_sl3_1km_ll.tif")
+s4 <- raster("../Data/SoilGrid/CECSOL_M_sl4_1km_ll.tif")
+plot(s1, maxpixels = 5000)
+
+# Calculating Cation Exchange Capacity for all depth until 30cm
+overlay(s1, s2, s3, s4, fun = function(a, b, c, d){(1/(30*2)*((5-0)*(a+b)+(15-5)*(b+c)+(30-15)*(c+d)))}, filename = "../Data/soil_30cm_250m", format = "GTiff", overwrite = TRUE)
+rfinal <- raster("soil_30cm_250m.tif")
+
+plot(rfinal, maxpixels = 5000)
---
+++ README.md
@@ -0,0 +1,9 @@
+# Repo created to host all scripts used in Socio Economic and Econological Restoration Meta Analysis
+
+## R  
+
+* [Cation Exchange Capacity](https://github.com/FelipeSBarros/RestorationMetaAnalysis/R/Calculating_SoilCEC.R)  
+
+## References  
+
+* **Cation Exchange Capacity:** [Hengl et al. 2017](http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0169748)  
