e9b043b2cf781a64a1f1c469af6daa77137714a5
Felipe Barros <felipe.b4rros@gmail.com>
2018-08-08 09:37:23 -0300

    added analysis vatidation

--- R/Calculating_SoilCEC.R
+++ R/Calculating_SoilCEC.R
@@ -9,7 +9,7 @@ s4 <- raster("../Data/SoilGrid/CECSOL_M_sl4_1km_ll.tif")
 plot(s1, maxpixels = 5000)
 
 # Calculating Cation Exchange Capacity for all depth until 30cm
-overlay(s1, s2, s3, s4, fun = function(a, b, c, d){(1/(30*2)*((5-0)*(a+b)+(15-5)*(b+c)+(30-15)*(c+d)))}, filename = "../Data/soil_30cm_250m", format = "GTiff", overwrite = TRUE)
-rfinal <- raster("soil_30cm_250m.tif")
+overlay(s1, s2, s3, s4, fun = function(a, b, c, d){(1/(30*2)*((5-0)*(a+b)+(15-5)*(b+c)+(30-15)*(c+d)))}, filename = "../Data/soil_CEC_30cm_1km", format = "GTiff", overwrite = TRUE)
+rfinal <- raster("../soil_CEC_30cm_1km.tif")
 
 plot(rfinal, maxpixels = 5000)
