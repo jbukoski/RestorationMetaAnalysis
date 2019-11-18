4dc28dbad0514466c6df67a36194b41ddee446b9
Felipe Barros <felipe.b4rros@gmail.com>
2019-11-18 10:49:15 -0300

    add BioVar calculation

--- R/ResultsMosaicking.R
+++ R/ResultsMosaicking.R
@@ -1,9 +1,13 @@
 library(gdalUtils)
 library(raster)
+library(tibble)
 
 # Mosaicking according to group analysis ----
 grupos <- list.files("../Data/Raster/Results", full.names = T)
 studyArea <- "../Data/Vector/StudyArea.shp"
+CalcBioVariation <- TRUE
+r.restorableAmount <- raster("../Data/Raster/Results/RestorableAmount/RestorableAmount_Merged.tif")
+
 for (folder in grupos){
   # folder <- grupos[1]
   
@@ -48,13 +52,35 @@ for (folder in grupos){
         cat("Normalizing raster values\n")
         r.landscape <- r.landscape/maxValue(r.landscape)
         
-      }
+        cat("Saving raster", name, "normalized \n")
+        writeRaster(r.landscape, 
+                    paste(folder,
+                          paste0(name, "_Norm.tif"), sep = "/"))
+        }
       
-      cat("Saving raster", name, "normalized \n")
-      writeRaster(r.landscape, 
-                  paste(folder,
-                        paste0(name, "_Norm.tif"), sep = "/"))
     }
+    
+    # test if Biodiversity variation recovery must be calculated
+    if (CalcBioVariation){
+      r.restorableAmount <- setMinMax(r.restorableAmount)
+      r.BioVar <- r.landscape/r.restorableAmount
+      r.BioVar <- r.BioVar/maxValue(r.BioVar)
+      
+      cat("Saving biodiversity recovery variation for", name, "\n")
+      writeRaster(r.BioVar, 
+                  paste(folder,
+                        paste0(name, "_BioVar.tif"), sep = "/"), overwrite = TRUE)
+      
+      # cat("Exporting biodiversity recovery variation results for", name, "\n")
+      # t.BioVar <- as_tibble( # no working yet
+      #   as.data.frame(r.BioVar))
+      # t.BioVar <- na.omit(t.BioVar)
+      # write_csv(t.BioVar, paste0("../Data/CSV/Results/", name, "_BioVar.csv"))
+      # 
+      # cat("Saving histogram for", name, "\n")
+      # ggplot(dat, aes(x = breaks, y = counts, fill =counts)) + ## Note the new aes fill here
+        # geom_bar(stat = "identity",alpha = 0.8)
+      }
 
   }
   
