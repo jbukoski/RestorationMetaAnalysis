661c3eec139b6d43e5ff6f0a57c9b5703932a459
Felipe Barros <felipe.b4rros@gmail.com>
2019-12-03 00:46:15 -0300

    update correlationSampling add samples CSV

--- R/CorrelationSampling.R
+++ R/CorrelationSampling.R
@@ -1,5 +1,20 @@
+library(raster)
+library(readr)
+library(dplyr)
+# library(ggplot2)
+
+# Getting all results
+AbundanceGrupos <- list.files("../Data/Raster/Results", pattern = "Abundance_Norm.tif$", recursive = TRUE, full.names = TRUE)
+RichnessGrupos <- list.files("../Data/Raster/Results", pattern = "ness_Norm.tif$", recursive = TRUE, full.names = TRUE)
+# stacking
+AbundanceLandscapeVarResults <- stack(AbundanceGrupos)
+RichnessLandscapeVarResults <- stack(RichnessGrupos)
+# stacking II
 stackedIndex <- stack(RichnessLandscapeVarResults, AbundanceLandscapeVarResults)
-samp <- sampleRandom(stackedIndex, 24000, xy = TRUE, sp=FALSE, na.rm = TRUE)
+
+# sampling
+set.seed(031219)
+samp <- sampleRandom(stackedIndex, 25000, xy = TRUE, sp=FALSE, na.rm = TRUE)
 head(samp)
 dim(samp)
 as_tibble(samp)
