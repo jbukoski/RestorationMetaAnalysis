32d0d6e3e291d389c24c4af76446982163944cf0
Felipe Barros <felipe.b4rros@gmail.com>
2019-12-15 11:52:40 -0300

    initial commit correlation functions

--- R/CorrelationFunction.R
+++ R/CorrelationFunction.R
@@ -4,7 +4,7 @@ sp.corrAnalysis <- function(r.model, npts, ...){
 
   # loading library
   library(raster)
-  library(dplyr)
+  library(readr)
   library(pgirmess)
   
   # output drectory
@@ -38,4 +38,99 @@ sp.corrAnalysis <- function(r.model, npts, ...){
   png(paste0(outDir, "/", modelName, "_moranCorrelog.png"))
   plot(modelCorrelog)
   dev.off()
+  }
+
+getDistance <- function(inputDir, modelName, suffix, ...){
+  # inputDir <- "./CorrelationResults"
+  # r.model1 = stack( list.files("../Data/Raster/Results", pattern = "Abundance_Norm.tif$|ness_Norm.tif$", recursive = TRUE, full.names = TRUE))
+  # modelName <- r.model1@layers[[1]]@data@names
+  # modelName1 <- r.model1@layers[[1]]@data@names
+  # modelName2 <- r.model2@layers[[1]]@data@names
+  # suffix = "_moranCorrelog.csv"
+  
+  # loading library
+  library(readr)
+  
+  # loading spatial correlation data
+  distCorr <- read_csv(
+    list.files(
+      inputDir, paste0(modelName, suffix), full.names = TRUE)
+    )
+  
+  # identifying min dist w/out sp correlation
+  distCorr <- distCorr[ min( which(distCorr$coef < 0)), 1 ]
+  return(distCorr)
+}
+
+fcn.name <- function(r.model1, r.model2, rmhmodel = FALSE){
+  # r.model1 = stack( list.files("../Data/Raster/Results", pattern = "Abundance_Norm.tif$|ness_Norm.tif$", recursive = TRUE, full.names = TRUE)[1])
+  # r.model2 = stack( list.files("../Data/Raster/Results", pattern = "Abundance_Norm.tif$|ness_Norm.tif$", recursive = TRUE, full.names = TRUE)[2])
+  
+  # loading library
+  library(dplyr)
+  library(spatstat)
+  
+  # defining input dir and model name
+  inputDir <- "./CorrelationResults"
+  modelName1 <- r.model1@layers[[1]]@data@names
+  modelName2 <- r.model2@layers[[1]]@data@names
+  
+  # loading spatial correlation data
+  distCorr1 <- getDistance("./CorrelationResults", r.model1@layers[[1]]@data@names,  "_moranCorrelog.csv")
+  distCorr2 <- getDistance("./CorrelationResults", r.model2@layers[[1]]@data@names,  "_moranCorrelog.csv")
+  
+  # identifying max dist w/out sp correlation
+  distCorrFinal <- max(distCorr1, distCorr2)
+  
+  # laoding world shapelie
+  library(sf)
+  library(spatstat)
+  
+  # world <- read_sf("/media/felipe/DATA/Proyectos/Dados/IPUMSI_world_release2017/world_countries_2017.shp")
+  
+ # plot(
+ #  st_bbox(
+ #   st_geometry(world)))
+  world.w <- owin(xrange=c(-122.1189, 180), yrange=c(-34.99998, 35), poly=NULL, mask=NULL,
+       unitname=NULL, xy=NULL)
+  
+  # Create an owin:
+  #world.w <- as.owin(
+  #  as_Spatial(
+  #    st_bbox(
+  #      world)))
+  # or
+  #world.w <- as(as_Spatial(world),"owin")
+  # plot(world.w)
+  
+  betaModel <- 10 # what is beta on hardcore model
+  t <- distCorrFinal/1
+  if ( rmhmodel ){
+    model <- rmhmodel(cif = "hardcore", 
+                      par = list(beta = betaModel, hc = t), 
+                      w = world.w)
+    X2 <- rmh(model)
+    #> Checking arguments..determining simulation windows...Starting simulation.
+    #> Initial state...Ready to simulate. Generating proposal points...Running Metropolis-Hastings.
+    
+  }
+  
+  
+  # plot(X2, main = paste("Approx. sim. of hardcore model; beta =", betaModel, "and R =", t))
+  # plot(r.model1, add=T)
+  
+  
+  #gerando os ptos regulares considerando os pontos aleatoriogerados anteriormente 'random'
+  # random <- rpoint(1000, giveup=2000, verbose=FALSE, nsim=1, drop=TRUE)
+  # plot(random)
+  
+  inibition_random <- rSSI( r = distCorrFinal/10, 
+                            n = 100, 
+                            giveup = 10, 
+                            nsim=1, 
+                            drop=TRUE,
+                            win = world.w)
+  # plot(inibition_random)
+  # plot(r.model1, add=T)
+  return(inibition_random)
   }
